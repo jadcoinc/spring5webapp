225688aacc11ae89150f0ad05043c612c7ff5a59
ed <ed@somecompany.com>
2019-12-31 20:35:53 -0500

    created a Books template

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -1,9 +1,11 @@
-package controllers;
+package guru.springframework.spring5webapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import guru.springframework.spring5webapp.model.Book;
 import guru.springframework.spring5webapp.repositories.BookRepository;
 
 @Controller
@@ -15,7 +17,7 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
-    @RequestMapping("/books")
+    @RequestMapping(value="/books", method=RequestMethod.GET)
     public String getBooks(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "books";
