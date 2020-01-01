0f1b929899159f7525582dfc6452db74dd9d49df
ed <ed@somecompany.com>
2019-12-31 20:57:06 -0500

    created Author controller and template

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import guru.springframework.spring5webapp.model.Book;
 import guru.springframework.spring5webapp.repositories.BookRepository;
 
 @Controller
